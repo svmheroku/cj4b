789dc8c37ce7e824bdbaaa99e30b9a04f538ed97
AudreyLeeMe <audreyleemee@yahoo.com>
2011-04-27 22:41:10 +0100

    c

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'ruby-debug19', :require => 'ruby-debug'
+
+gem 'nokogiri'
+
---
+++ html.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# html.bash
+
+# I use this script to generate views files (HTML mostly).
+
+cd /pt/s/rl/cj/
+./predictions/fx_past/index.bash
+# ./predictions/us_stk_past/index.bash
+
+exit 0
