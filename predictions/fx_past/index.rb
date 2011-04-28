674544507db83881e605ec5930ba1992a81a78f8
AudreyLeeMe <audreyleemee@yahoo.com>
2011-04-28 17:39:13 +0100

    cj/predictions/fx_past/index.rb

--- predictions/fx_past/index.rb
+++ predictions/fx_past/index.rb
@@ -3,7 +3,7 @@
 require "rubygems"
 require "bundler/setup"
 require "nokogiri"
-require "ruby-debug"
+# require "ruby-debug"
 
 myf = File.open("tmp.html")
 
@@ -13,8 +13,6 @@ myf.close
 
 td_elems = html_doc.search("td")
 
-debugger
-
 sz = td_elems.size
 
 # Insert links inside each td-element:
@@ -22,6 +20,12 @@ td_elems.each{|td|
   # Change Week: 2011-01-31 Through 2011-02-04
   # to
   # /predictions/fx_past_wk2011_01_31
-  debugger
   hhref = td.inner_html
+  hhref = td.inner_html.gsub(/\n/,'')
+  hhref = td.inner_html.gsub(/\n/,'').sub(/Week: /,'').sub(/ Through .*$/,'').gsub(/-/,'_')
+  hhref="/predictions/fx_past_wk#{hhref}"
+  td.inner_html = "<a href='#{hhref}'>#{td.inner_html.gsub(/\n/,'')}</a>"
 }
+
+# Im done, print it now so my shell can redirect the HTML into a file:
+print html_doc.search("table#table_fx_past").to_html
