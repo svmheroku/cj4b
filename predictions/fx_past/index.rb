dc2bbedeaee98174817b46cc5be3bbca4a34c2b2
AudreyLeeMe <audreyleemee@yahoo.com>
2011-04-28 17:12:46 +0100

    c

--- predictions/fx_past/index.rb
+++ predictions/fx_past/index.rb
@@ -5,22 +5,23 @@ require "bundler/setup"
 require "nokogiri"
 require "ruby-debug"
 
-debugger
-
-x = 1
-
-# doc = open("tmp.html") { |f| Hpricot(f) }
-
 myf = File.open("tmp.html")
 
 html_doc = Nokogiri::HTML(myf)
 
 myf.close
 
-mth = html_doc.methods
-
-# p mth.to_s
-
 td_elems = html_doc.search("td")
 
+debugger
+
 sz = td_elems.size
+
+# Insert links inside each td-element:
+td_elems.each{|td|
+  # Change Week: 2011-01-31 Through 2011-02-04
+  # to
+  # /predictions/fx_past_wk2011_01_31
+  debugger
+  hhref = td.inner_html
+}
