3994f093335d72bf533a94470509ec3b938b4b7b
AudreyLeeMe <audreyleemee@yahoo.com>
2011-05-08 16:57:45 +0100

    /pt/s/rl/cj/.cj

---
+++ predictions/fx_past/index_spec.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# index_spec.bash
+
+# I use this script as a shell wrapper for index_spec.rb
+
+# This script depends on an rvm gemset:
+# rvm use ruby-1.9.2-head@gs1 --default
+
+. /pt/s/rl/cj/.cj
---
+++ predictions/fx_past/index_spec.rb
@@ -0,0 +1,8 @@
+#
+# index_spec.rb
+#
+
+# Eventually this script will replace index.bash
+
+require 'spec_helper'
+
